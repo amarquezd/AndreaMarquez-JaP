e57773475ac73e60c6cccd38d94d602a3e14ae40
amarquezd <88244802+amarquezd@users.noreply.github.com>
2021-08-20 12:08:11 -0300

    ...

--- js/products.js
+++ js/products.js
@@ -1,6 +1,34 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function (e) {
+
+
+document.addEventListener("DOMContentLoaded", function () {
+
+    fetch(PRODUCTS_URL)
+    
+    .then(response => response.json())
+
+    .then(data => {
+        
+        data.forEach(data => {
+
+            let info=""
+
+            info=
+            `
+            <tr>
+            <td>` + data.marca + `</td>
+            <td>` + data.modelo + `</td>
+            <td>` + data.color + `</td>
+            </tr>
+            `;
+
+            document.getElementsByClassName("products").innerHTML += info;
+            
+        });
+    })
+
+    .catch (error => alert("Hubo un error!:" + error));
 
 });
