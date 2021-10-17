7b7696999e597a6f8f2a546e22672e17c161057e
amarquezd <88244802+amarquezd@users.noreply.github.com>
2021-10-17 19:37:48 -0300

    entrega 5

--- js/cart.js
+++ js/cart.js
@@ -2,13 +2,32 @@
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 const CART_PRODS = "https://japdevdep.github.io/ecommerce-api/cart/654.json"
+let productos
 
-function subtotal(i) {
-  let cantidad = parseInt(document.getElementById(`quantity${i}`).value);
-  let precio = parseInt(document.getElementById(`precioitem${i}`).innerHTML);
+function subtotal(numberOfArticles) {
+  let totalPesos = 0;
+  let totalDolares = 0
 
-  let subtotalItem = cantidad * precio;
-  document.getElementById(`subtitem${i}`).innerHTML = subtotalItem;
+  for (let i = 0; i < numberOfArticles; i++) {
+
+    let cantidad = parseInt(document.getElementById(`quantity${i}`).value);
+    let precio = parseInt(document.getElementById(`precioitem${i}`).innerHTML);
+
+    let subtotalItem = cantidad * precio;
+
+    if (productos[i].currency != "UYU") {
+      totalPesos += subtotalItem * 40;
+      totalDolares += subtotalItem;
+    } else {
+      totalPesos += subtotalItem;
+      totalDolares += subtotalItem / 40;
+    }
+
+    document.getElementById(`subtitem${i}`).innerHTML = subtotalItem;
+  }
+
+  document.getElementById(`precioUyu`).innerHTML = `UYU `+ totalPesos;
+  document.getElementById(`precioUsd`).innerHTML = `USD `+ totalDolares;
 }
 
 document.addEventListener("DOMContentLoaded", function (e) {
@@ -17,48 +36,45 @@ document.addEventListener("DOMContentLoaded", function (e) {
     .then(response => response.json())
     .then(data => {
 
-      let cartInfo = "";
+      productos = data.articles;
 
       for (let i = 0; i < data.articles.length; i++) {
 
         let item = data.articles[i];
-        let subtotal = data.articles[i].count * data.articles[i].unitCost;
 
-        /*let subtotal = document.getElementById("subtItem").addEventListener("change", subtotal())*/
-        /*data.articles[i].unitCost * data.articles[i].count;*/
+        const row = `<div class="row mt-4 ml-1">
+       <div class="col-sm-2 col-md-3 col-lg-3 ">
+         <img src="${item.src}" alt="" class="img-thumbnail">
+       </div>
+       <div class="col-sm-7 col-md-4 col-lg-5">
+         <small class="text-muted">Item</small>
+         <h6>${item.name}</h6>
+         <small class="text-muted">Cant.</small><br>
+         <div>
+           <input type="number" id="quantity${i}" name="cantidad" min="1" max="5" class=" btn-outline-secondary" value="${item.count}">
+         </div>
+       </div>
+       <div class="col-sm-6 col-md-2 col-lg-2">
+         <h6 class="text-muted">Precio: </h6>
+         <h4>${item.currency}</h4><h4 id="precioitem${i}">${item.unitCost}</h4>
+       </div>
+       <div class="col-sm-6 col-md-2 col-lg-2">
+         <h6 class="text-muted">Subtotal: </h6>
+         <h3>${item.currency}</h3><h3 id="subtitem${i}"></h3>
+       </div>
+     </div>
+     <hr>`
 
-        cartInfo += `<div class="row mt-4 ml-1">
-                <div class="col-sm-2 col-md-3 col-lg-3 ">
-                  <img src="${item.src}" alt="" class="img-thumbnail">
-                </div>
-                <div class="col-sm-7 col-md-4 col-lg-5">
-                  <small class="text-muted">Item</small>
-                  <h6>${item.name}</h6>
-                  <small class="text-muted">Cant.</small><br>
-                  <div>
-                    <input type="number" id="quantity${i}" name="cantidad" min="1" max="5" class=" btn-outline-secondary" value="${item.count}">
-                  </div>
-                </div>
-                <div class="col-sm-6 col-md-2 col-lg-2">
-                  <h6 class="text-muted">Precio: </h6>
-                  <h4>${item.currency}</h4><h4 id="precioitem${i}">${item.unitCost}</h4>
-                </div>
-                <div class="col-sm-6 col-md-2 col-lg-2">
-                  <h6 class="text-muted">Subtotal: </h6>
-                  <h3>${item.currency}</h3><h3 id="subtitem${i}"></h3>
-                </div>
-              </div>
-              <hr>`
+        document.getElementById("cart_info").innerHTML += row;
 
       }
-
-      document.getElementById("cart_info").innerHTML = cartInfo;
-
 
       for (let i = 0; i < data.articles.length; i++) {
-        document.getElementById(`quantity${i}`).addEventListener("change", () => subtotal(i));
-        subtotal(i);
+        document.getElementById(`quantity${i}`).addEventListener("change", () => subtotal(data.articles.length));
+        subtotal(data.articles.length);
       }
+
+      document.getElementById('numberOfItems').innerHTML = data.articles.length +` items`;
     })
 
 
